0528787896e958513cb1bd84ac9f9efee2543104
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-12-03 19:27:35 +0600

    add computer 3d model

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import {
 
 function App() {
   return (
-    <div className="relative z-0 bg-primary w-screen h-screen">
+    <div className="relative z-0 bg-primary">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
         <Hero />
